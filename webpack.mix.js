6723e0993fb8c8318f37ab2478b4248bd47344ab
Daniel Bachhuber <daniel@bachhuber.co>
2021-04-09 06:50:46 -0700

    Add a stylesheet to better emulate production

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-tailwind');
 
+mix.sass('assets/scss/style.scss', 'assets/dist/');
+
 mix.js('assets/js/app.js', 'assets/dist').vue({ version: 2 });
 
 mix.tailwind('./tailwind.config.js');
